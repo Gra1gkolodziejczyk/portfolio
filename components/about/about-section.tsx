08199b6ae9873e258e2e130b3a6a72916943a6ed
Gra1gKolodziejczyk <graig.kolodziejczyk@ynov.com>
2025-06-03 15:50:14 +0200

    resolv conflict naming image

--- components/about/about-section.tsx
+++ components/about/about-section.tsx
@@ -1,13 +1,13 @@
 'use client';
 
-import {motion} from "framer-motion";
-import Image from "next/image";
-import IntelliJ from "@/public/intelliJ.svg";
-import Valise from "@/public/valise.png";
-import Link from "next/link";
-import Linkedin from "@/public/linkedin.svg";
-import Bitcoin from "@/public/bitcoin.svg";
-import Pepper from "@/public/pepper.png";
+import { motion } from 'framer-motion';
+import Image from 'next/image';
+import IntelliJ from '@/public/intellij.svg';
+import Valise from '@/public/valise.png';
+import Link from 'next/link';
+import Linkedin from '@/public/linkedin.svg';
+import Bitcoin from '@/public/bitcoin.svg';
+import Pepper from '@/public/pepper.png';
 
 export function AboutSection() {
   return (
@@ -16,24 +16,27 @@ export function AboutSection() {
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5 }}
-        className="text-3xl sm:text-5xl font-bold text-primary mb-8"
+        className='text-3xl sm:text-5xl font-bold text-primary mb-8'
       >
         À propos de moi
       </motion.h2>
 
-      <div className="grid gap-6">
+      <div className='grid gap-6'>
         <motion.div
           initial={{ opacity: 0, x: -20 }}
           animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 0.5, delay: 0.2 }}
-          className="flex items-start gap-4"
+          className='flex items-start gap-4'
         >
-          <Image src={IntelliJ} alt="" height={35} width={35}/>
+          <Image src={IntelliJ} alt='' height={35} width={35} />
           <div>
-            <h3 className="text-xl font-semibold mb-2 text-black dark:text-white">Passionné de Développement</h3>
-            <p className="text-muted-foreground">
-              Je suis un développeur passionné par les nouvelles technologies et l&apos;innovation.
-              Mon enthousiasme pour le code me pousse à constamment explorer et apprendre de nouvelles choses.
+            <h3 className='text-xl font-semibold mb-2 text-black dark:text-white'>
+              Passionné de Développement
+            </h3>
+            <p className='text-muted-foreground'>
+              Je suis un développeur passionné par les nouvelles technologies et
+              l&apos;innovation. Mon enthousiasme pour le code me pousse à
+              constamment explorer et apprendre de nouvelles choses.
             </p>
           </div>
         </motion.div>
@@ -42,15 +45,21 @@ export function AboutSection() {
           initial={{ opacity: 0, x: -20 }}
           animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 0.5, delay: 0.4 }}
-          className="flex items-start gap-4"
+          className='flex items-start gap-4'
         >
-          <Image src={Valise} alt="" height={35} width={35} />
+          <Image src={Valise} alt='' height={35} width={35} />
           <div>
-            <h3 className="text-xl font-semibold mb-2 text-black dark:text-white">Freelance & Entrepreneur</h3>
-            <p className="text-muted-foreground">
-              Je mène une double vie professionnelle en tant que freelance et entrepreneur,
-              toujours à la recherche de nouveaux défis et opportunités. Plusieurs projets à mon actif !
-              (voir <Link href="#projets" className="not-active:text-primary">mes projets</Link>)
+            <h3 className='text-xl font-semibold mb-2 text-black dark:text-white'>
+              Freelance & Entrepreneur
+            </h3>
+            <p className='text-muted-foreground'>
+              Je mène une double vie professionnelle en tant que freelance et
+              entrepreneur, toujours à la recherche de nouveaux défis et
+              opportunités. Plusieurs projets à mon actif ! (voir{' '}
+              <Link href='#projets' className='not-active:text-primary'>
+                mes projets
+              </Link>
+              )
             </p>
           </div>
         </motion.div>
@@ -59,14 +68,18 @@ export function AboutSection() {
           initial={{ opacity: 0, x: -20 }}
           animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 0.5, delay: 0.6 }}
-          className="flex items-start gap-4"
+          className='flex items-start gap-4'
         >
-          <Image src={Linkedin} alt="" height={35} width={35}/>
+          <Image src={Linkedin} alt='' height={35} width={35} />
           <div>
-            <h3 className="text-xl font-semibold mb-2 text-black dark:text-white">En Recherche Active</h3>
-            <p className="text-muted-foreground">
-              Actuellement à la fin de mes études en master 2 expert en développement logiciel, je recherche un CDI pour démarrer une nouvelle aventure
-              professionnelle et mettre à profit mes compétences.
+            <h3 className='text-xl font-semibold mb-2 text-black dark:text-white'>
+              En Recherche Active
+            </h3>
+            <p className='text-muted-foreground'>
+              Actuellement à la fin de mes études en master 2 expert en
+              développement logiciel, je recherche un CDI pour démarrer une
+              nouvelle aventure professionnelle et mettre à profit mes
+              compétences.
             </p>
           </div>
         </motion.div>
@@ -75,14 +88,17 @@ export function AboutSection() {
           initial={{ opacity: 0, x: -20 }}
           animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 0.5, delay: 0.8 }}
-          className="flex items-start gap-4"
+          className='flex items-start gap-4'
         >
-          <Image src={Bitcoin} alt="" height={35} width={35}/>
+          <Image src={Bitcoin} alt='' height={35} width={35} />
           <div>
-            <h3 className="text-xl font-semibold mb-2 text-black dark:text-white">Trader Passionné</h3>
-            <p className="text-muted-foreground">
-              Depuis 2020, j&apos;investis dans le Bitcoin et je trade le Forex (EUR-USD / Gold),
-              alliant analyse technique et gestion des risques.
+            <h3 className='text-xl font-semibold mb-2 text-black dark:text-white'>
+              Trader Passionné
+            </h3>
+            <p className='text-muted-foreground'>
+              Depuis 2020, j&apos;investis dans le Bitcoin et je trade le Forex
+              (EUR-USD / Gold), alliant analyse technique et gestion des
+              risques.
             </p>
           </div>
         </motion.div>
@@ -91,18 +107,21 @@ export function AboutSection() {
           initial={{ opacity: 0, x: -20 }}
           animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 0.5, delay: 1.0 }}
-          className="flex items-start gap-4"
+          className='flex items-start gap-4'
         >
-          <Image src={Pepper} alt="" height={35} width={35} />
+          <Image src={Pepper} alt='' height={35} width={35} />
           <div>
-            <h3 className="text-xl font-semibold mb-2 text-black dark:text-white">Amateur de Cuisine Épicée</h3>
-            <p className="text-muted-foreground">
-              Grand amateur de cuisine épicée, j&apos;aime découvrir de nouvelles saveurs
-              et expérimenter avec différents styles culinaires.
+            <h3 className='text-xl font-semibold mb-2 text-black dark:text-white'>
+              Amateur de Cuisine Épicée
+            </h3>
+            <p className='text-muted-foreground'>
+              Grand amateur de cuisine épicée, j&apos;aime découvrir de
+              nouvelles saveurs et expérimenter avec différents styles
+              culinaires.
             </p>
           </div>
         </motion.div>
       </div>
     </>
-  )
+  );
 }
